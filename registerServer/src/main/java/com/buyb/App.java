2414828c191fd1c2e82535ff6e652253a6e31a28
buyabo <buyb@mti-sh.cn>
2020-03-24 14:20:59 +0800

    注册中心

--- registerServer/src/main/java/com/buyb/App.java
+++ registerServer/src/main/java/com/buyb/App.java
@@ -1,13 +1,19 @@
 package com.buyb;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 /**
  * Hello world!
  *
  */
+@SpringBootApplication
+@EnableEurekaServer
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SpringApplication.run(App.class, args);
     }
 }
